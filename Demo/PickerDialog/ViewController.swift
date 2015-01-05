d0754a82d5386a92ed30bcbc17e201bd4da6e945
vinicius <vinicius@viniciuss-Mac.local>
2015-01-05 14:49:42 -0200

    Version 1.0 Stable

--- Demo/PickerDialog/ViewController.swift
+++ Demo/PickerDialog/ViewController.swift
@@ -1,38 +1,20 @@
-//
-//  ViewController.swift
-//  DatePickerDialogExample
-//
-//  Created by vinicius on 11/20/14.
-//  Copyright (c) 2014 Squimer. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController, DatePickerDialogDelegate {
-    
-    var datePickerDialog: DatePickerDialog!
+class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
     /* Overrides */
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.datePickerDialog = DatePickerDialog(frame: self.view.frame)
-        self.datePickerDialog.delegate = self
-        self.view.addSubview(datePickerDialog)
-    }
-    
-    /* DatePickerDialogDelegate */
-    func datePickerDialog(didSelect date: NSDate, from tag: String) {
-        if tag == "myTextFieldTag" {
-            textField.text = "\(date)"
-        }
     }
     
     /* IBActions */
     @IBAction func datePickerTapped(sender: AnyObject) {
-        self.datePickerDialog.showPickerWithTag("myTextFieldTag")
+        DatePickerDialog().show(title: "DatePickerDialog", doneButtonTitle: "Done", cancelButtonTitle: "Cancel", datePickerMode: .Date) {
+            (date) -> Void in
+            self.textField.text = "\(date)"
+        }
     }
     
 }
