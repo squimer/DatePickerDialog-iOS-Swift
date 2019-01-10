3afbaef17ed756270604e5a64fa68ff569d97ff1
Andre Nguyen <nguyen.andres@gmail.com>
2019-01-10 18:58:37 +0100

    Update DatePickerDialog.swift
    update size of dialog view

--- Sources/DatePickerDialog.swift
+++ Sources/DatePickerDialog.swift
@@ -159,7 +159,7 @@ open class DatePickerDialog: UIView {
     /// Creates the container view here: create the dialog, then add the custom content and buttons
     private func createContainerView() -> UIView {
         let screenSize = UIScreen.main.bounds.size
-        let dialogSize = CGSize(width: 300, height: 230 + kDefaultButtonHeight + kDefaultButtonSpacerHeight)
+        let dialogSize = CGSize(width: UIScreen.main.bounds.size.width - 10, height: 230 + kDefaultButtonHeight + kDefaultButtonSpacerHeight)
 
         // For the black background
         self.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
